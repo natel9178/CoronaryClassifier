1f523717aeae5e71882b8525e9af1034f5f55013
Nate Lee <natelee@rice12.stanford.edu>
2018-09-08 17:07:22 -0700

    added more metrcs

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 module load cudnn
-python3 train.py
+python3 train.py --restore_from "experiments/weights/epochs_200_preprocess_testing_with_regularization_loss_2018-09-08_12-11-33_weights.chkpt.hdf5" --starting_epoch 139
